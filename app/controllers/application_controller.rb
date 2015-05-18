00123b708ac5de3d43a87ff8877605716205aaaf
Eschults <edward.schults@gmail.com>
2015-05-18 21:58:01 +0200

    fixing after_sign_in --attempt #2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    if (request.env['omniauth.origin'] && resource.digicode.nil?
+    if (request.env['omniauth.origin'] && resource.digicode.nil?)
       edit_user_path(resource)
     else
       users_path
