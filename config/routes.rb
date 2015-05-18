68f297a1f60a19d90c27da2a46ebf4595d6d3540
Eschults <edward.schults@gmail.com>
2015-05-18 14:22:02 +0200

    back-end almost ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+    member do
+      post :ask_for_code, to: "users#ask_for_code"
+      post :accept_request, to: "users#accept_request"
+      post :decline_request, to: "users#decline_request"
+    end
+  end
 end
